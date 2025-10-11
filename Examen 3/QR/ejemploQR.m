3e1719c7b6fb10b9dc279ee2e23d91c225b25764
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2025-10-11 14:13:21 -0600

    Update ejemploQR.m

--- Examen 3/QR/ejemploQR.m
+++ Examen 3/QR/ejemploQR.m
@@ -1,13 +1,16 @@
 %Ejemplo metodo QR
-
+clc;clear all;
 A=[2 2 2 1;2 0 2 0;2 2 0 2;1 0 2 2];
 
 iterMax=1000;
 tol=1e-10;
 
+m=size(A,1); %#Filas de A
+Im=eye(m); %identidad
+
 Ak=A;
 dk=diag(Ak);
-Uk=eye(4);
+Uk=eye(4); %identidad
 
 for k=1:iterMax
   [Qk,Rk]=qr(Ak);
@@ -22,7 +25,7 @@ for k=1:iterMax
   dk=dkN;
 end
 
-dk
-Uk
+dk  %valores propios
+Uk  %vectores propios son las columnas
 k
 erk
