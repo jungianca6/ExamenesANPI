430751f6769773ec3c4816c6099602f2400d12f8
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2025-10-15 22:00:04 -0600

    Rk, predictor y taylor

--- Examen4/MetodosIterativos/main.m
+++ Examen4/MetodosIterativos/main.m
@@ -1,11 +1,15 @@
 %Comparacion de metodos
 clc; clear; close all
+pkg load symbolic  % Cargar paquete simbólico
+
+syms x y
+
 
 f=@(x,y) x+y;
 a=0;
 b=1;
 y0=1;
-n=6;
+n=60;
 
 %Gráfica de la solución
 hold on
@@ -14,10 +18,8 @@ yDisct=2*exp(xDisct)-xDisct-1;
 hold on
 plot(xDisct,yDisct,'b')
 
-[x_1,y_1]=metodo_euler(f,a,b,y0,n);
+[x_1,y_1]=metodo_taylor(f,a,b,y0,n,5);
 stem(x_1,y_1,'r')
 
-[x_2,y_2]=metodo_rk3(f,a,b,y0,n);
-stem(x_2,y_2,'g')
 
-legend('Solucion Analitica','Euler','RK3')
+legend('Solucion Analitica','Taylor')
--- Examen4/MetodosIterativos/metodo_rk2.m
+++ Examen4/MetodosIterativos/metodo_rk2.m
@@ -1,4 +1,4 @@
-function [x,y]=metodo_rk3(f,a,b,y0,n)
+function [x,y]=metodo_rk2(f,a,b,y0,n)
   h=(b-a)/n;
   x=a:h:b;
   y=zeros(1,n+1);
@@ -6,9 +6,6 @@ function [x,y]=metodo_rk3(f,a,b,y0,n)
   for k=1:n
     K1=f(x(k),y(k));
     K2=f(x(k)+h/2,y(k)+(h/2)*K1);
-    K3=f(x(k)+h,y(k)-h*K1+2*h*K2);
-    y(k+1)=y(k)+(h/6)*(K1+4*K2+K3);
+    y(k+1)=y(k)+h*K2;
   end
 end
-
-
